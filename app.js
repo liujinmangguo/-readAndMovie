83c83216b47b84e88fa48668f772b3f1729286ed
liujinmangguo <liujinmangguo@gmail.com>
2017-02-10 20:09:14 +0800

    app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+App({
+    globalData : {
+        g_isPlayingMusic : false,
+        doubanBase : "https://api.douban.com",
+    }
+})
+// App({
+//   onLaunch: function () {
+//     console.log(onLaunch);
+//   },
+//   onShow: function () {
+//     console.log(onLoad);
+//   },
+//   onHide: function () {
+//     console.log(onHide);
+//   }
+  
+// })
